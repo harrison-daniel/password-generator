1ff92ba2b5d68eb2e4baff1b6ff5294fc8b10467
Harrison Daniel <hd4n@outlook.com>
2022-03-19 13:27:26 -0400

    initial outline and password variables

--- Develop/script.js
+++ Develop/script.js
@@ -2,7 +2,69 @@
 
 
 // Get references to the #generate element
+//var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+//function writePassword() {
+  //var password = generatePassword();
+  //var passwordText = document.querySelector("#password");
+
+  //passwordText.value = password;
+
+//}
+
+// Add event listener to generate button
+//generateBtn.addEventListener("click", writePassword);
+
+// end starter code
+//------------------------------------------
+
+  var specialChars = " `~!@#$%^&*()_-+=";
+  var numericChars = "0123456789";
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHUJKLMNOPQRSTUVWXYZ";
+
+
+// Assignment code here
+
+function generatePassword() {
+  console.log("button clicked");
+  
+  // prompt user for password criteria
+
+  // password length
+  var passwordLength = window.prompt ("How many characters would you like your password to be?");
+  if (passwordLength < 8) {
+    alert ("Please enter a number between 8 and 128");
+    return;
+  }
+  if (passwordLength > 128); {
+    alert ("Please enter a number between 8 and 128");
+    return;
+  }
+  // confirm special characters
+
+  // confirm numeric characters
+
+  // comfirm lowercase characters
+
+  // confirm uppercase characters
+      
+    
+// validate input
+
+
+// generated password output
+
+
+  return "generated password will go here"
+}
+
+// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+
+
 
 // Write password to the #password input
 function writePassword() {
