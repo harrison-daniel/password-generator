d31863568ff881f6348ad8b9dacc142cf573a229
Harrison Daniel <hd4n@outlook.com>
2022-03-20 15:33:20 -0400

    Working Prompts / Alerts for User Pass Length Input

--- Develop/script.js
+++ Develop/script.js
@@ -19,42 +19,64 @@
 // end starter code
 //------------------------------------------
 
-  var specialChars = " `~!@#$%^&*()_-+=";
-  var numericChars = "0123456789";
-  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
-  var uppercaseChars = "ABCDEFGHUJKLMNOPQRSTUVWXYZ";
+  var specialCharsArr = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?", "<", ">"];
+  var numericChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var lowercaseChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var uppercaseChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  
+  //var maxPasswordLength = 128;
+  //var minPasswordLength = 8;
+  
 
+ 
 
 // Assignment code here
 
 function generatePassword() {
   console.log("button clicked");
-  
-  // prompt user for password criteria
 
-  // password length
-  var passwordLength = window.prompt ("How many characters would you like your password to be?");
-  if (passwordLength < 8) {
-    alert ("Please enter a number between 8 and 128");
-    return;
-  }
-  if (passwordLength > 128); {
-    alert ("Please enter a number between 8 and 128");
-    return;
-  }
-  // confirm special characters
+  // PROMPT USER FOR PASS CRITERIA
 
-  // confirm numeric characters
+  // 1) Ask user for character length
+  var userCharsLength = window.prompt ("How many characters would you like your password to be? (Must be between 8 and 128 characters long");
 
-  // comfirm lowercase characters
+  // Validate character length is between 8-128
+    while (userCharsLength <= 7 || userCharsLength >= 129 || isNaN (userCharsLength) || userCharsLength === "" ) {
+      window.alert("Please enter a valid number between 8 and 128");
 
-  // confirm uppercase characters
+      var userCharsLength = window.prompt ("How many characters would you like your password to be? (Must be between 8 and 128 characters long");
+    }
+    // log character length
+    console.log("password length is" + userCharsLength)
+
+    //  2) Ask user for special characters
+    var userSpecialChars = window.confirm ("Click OK to confirm including Special characters.");
+
+    console.log("Use Special Characters" + userSpecialChars);
+    // Confirm Special Characters or not
+
+    // user selection - numeric characters
+    var userNumericChars = window.confirm ("Click OK to confirm including Numeric characters.");
+    console.log("Use Numeric Characters" + userNumericChars);
+    // confirm numeric characters
+
+
+    // user selection - lowercase characters
+    var userLowercaseChars = window.confirm ("Click OK to confirm including Lowercase characters.");
+    console.log("USe Lowercase Characters" + userLowercaseChars);
+    // comfirm lowercase characters
+
+
+    // user selection - uppercase characters
+    var userUppercaseChars = window.confirm ("Click OK to confirm including Uppercase characters.");
+    console.log("Use Uppercase Characters" + userUppercaseChars);
+    // confirm uppercase characters
       
     
-// validate input
+  // validate input
+    
 
-
-// generated password output
+  // generated password output
 
 
   return "generated password will go here"
@@ -62,9 +84,6 @@ function generatePassword() {
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
-
-
 
 // Write password to the #password input
 function writePassword() {
